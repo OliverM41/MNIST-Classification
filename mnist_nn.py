0af5149428155bc75d4d07004b89a328e82ff543
OliverM41 <olivermorales4117@gmail.com>
2024-12-23 12:00:27 -0500

    reorganized files

--- mnist_nn.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f4d067335f87ab8df88ffda85ff69cc54fbbf0a31cc025f2ca33ac6c9163701e
-size 6313
---
+++ src/mnist_nn.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad89fcf570317ecf122b70fd127358458dbb755a2e534ce4fc6fb175502c352e
+size 6318
