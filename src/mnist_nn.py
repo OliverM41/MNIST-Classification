656ca5c66aec8192007795f13ec7f3eaa47b0e56
OliverM41 <olivermorales4117@gmail.com>
2024-12-23 14:48:10 -0500

    added pyplot graphs of accuracy and cost

--- src/mnist_nn.py
+++ src/mnist_nn.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:233dea9a6f3ecacc4f0d8e9fc7f87a1d7ff9b8868c69dd7f5e7d0a0f249c6c29
-size 6500
+oid sha256:d9cf244c6091c36a8677522632bf019fffb5303d8ed9a8e0bba13e949ed6595d
+size 6793
