ca3b821b3f064568f6945ebaed4a74dbfbdbe8e0
OliverM41 <olivermorales4117@gmail.com>
2024-12-24 15:43:01 -0500

    added cross validation test

--- src/mnist_nn.py
+++ src/mnist_nn.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9cf244c6091c36a8677522632bf019fffb5303d8ed9a8e0bba13e949ed6595d
-size 6793
+oid sha256:8caabe0bae24d000ae741ad050c57cfc5031182b07b674edad0a846eeb0e57c5
+size 8701
