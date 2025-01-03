7d738a626ec18847e13df065e5af964c180a6c0a
OliverM41 <olivermorales4117@gmail.com>
2025-01-02 21:06:50 -0500

    removing lfs temporarily

--- src/Layer.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:61e139ae7129c8264d7fe1742df82beee3b57b9288908f2de94a14ce33f86c3b
-size 2184
--- src/mnist_nn.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ed84a10ab966abec9e0a31e8d71af44db6962c7e598234986c063bcc1ce9b2de
-size 9901
